fcff5b652ddf22a72733efe028c19e1ddef5cd42
eman1833 <eman.mohamed000000000000@gmail.com>
2022-07-07 14:47:39 +0200

    modifing by symptoms

--- backend/test.py
+++ backend/test.py
@@ -12,6 +12,11 @@ class Patient(db.Model):
      name = db.Column(db.String(50),nullable=false)
      age = db.Column(db.String(255))
      phone=db.Column(db.Integer)
+     h1=db.Column(db.String)
+     h2=db.Column(db.String)
+     h3=db.Column(db.String)
+     h4=db.Column(db.String)
+     h5=db.Column(db.String)
      Albumin=db.Column(db.Integer)
      Platelets=db.Column(db.Integer)
      AFP=db.Column(db.Integer)
@@ -20,7 +25,8 @@ class Patient(db.Model):
      Iron=db.Column(db.Integer)
     #  result1=db.Column(db.Integer)
      def __repr__(self):
-        return f"""{self.name}-{self.age}-{self.phone}{self.AFP}{self.MCV}{self.Platelets}{self.Albumin}
+        return f"""{self.name}-{self.age}-{self.phone}{self.AFP}{self.h1}{self.h2}{self.h3}{self.h4}{self.h5}
+        {self.MCV}{self.Platelets}{self.Albumin}
         {self.ALP}{self.Iron}
         """
 @app.route("/", methods=["POST", "GET"])
@@ -43,7 +49,8 @@ def get_id():
      output=[]
      for patient in patients:
         pat_data= {'id':patient.id,'name':patient.name,'age':patient.age,
-        'phone':patient.phone,'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,
+        'phone':patient.phone,'h1':patient.h1,'h2':patient.h2,'h3':patient.h3,'h4':patient.h4,'h5':patient.h5,
+        'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,
          'ALP':patient.ALP,'MCV':patient.MCV, 'Iron':patient.Iron}
         output.append(pat_data)
      return {"patients" :output}
@@ -55,26 +62,65 @@ def get_pat(id):
          return{"error":"not found"}
     else:     
        return {'id':patient.id,'name':patient.name,'age':patient.age,
-        'phone':patient.phone,'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,
+        'phone':patient.phone,'h1':patient.h1,'h2':patient.h2,'h3':patient.h3,'h4':patient.h4,'h5':patient.h5,
+        'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,
          'ALP':patient.ALP,'MCV':patient.MCV, 'Iron':patient.Iron}
+
 @app.route("/patient", methods=["POST"])  
 def add_pat():
+    counter=0
     id=request.json['id']
     patient=Patient.query.get(id)
     if request.method == "POST":
-       
+        
         if patient is None:
+                h1=request.json['h1']
+                if h1 =="yes":
+                    counter+=1
+                    
+                else:
+                    counter
+
+                h2=request.json['h2'] 
+                if h2 == "yes":
+                    counter+=1
+                    counter
+                else:
+                    counter 
+                h3=request.json['h3'] 
+                if h3 =="yes":
+                    counter+=1
+                    counter
+                else:
+                    counter 
+                h4=request.json['h4'] 
+                if h4 =="yes":
+                    counter+=1
                 
-                patient=Patient (id=request.json['id'],name=request.json['name'],age=request.json['age'],phone=request.json['phone'], 
-                Albumin=request.json['Albumin'], Platelets=request.json['Platelets'],
-               AFP=request.json['AFP'], ALP=request.json['ALP'],MCV=request.json['MCV'],
-               Iron=request.json['Iron'] )
-                db.session.add(patient) 
-                db.session.commit()
-                return{'id':patient.id,"massage":"the patient is added"}
+                else:
+                    counter 
+                h5=request.json['h5'] 
+                if h5=="yes":
+                    counter+=1
+                    
+                else:
+                    counter
+                print(counter)     
+                if counter>=3:
+
+                                patient=Patient (id=request.json['id'],name=request.json['name'],age=request.json['age'],phone=request.json['phone'], 
+                                h1=request.json['h1'], h2=request.json['h2'],h3=request.json['h3'],h4=request.json['h4'],h5=request.json['h5'],
+                                Albumin=request.json['Albumin'], Platelets=request.json['Platelets'],
+                            AFP=request.json['AFP'], ALP=request.json['ALP'],MCV=request.json['MCV'],
+                            Iron=request.json['Iron'] )
+                                db.session.add(patient) 
+                                db.session.commit()
+                                return{'id':patient.id,"massage":"the patient is added"}
+                               
+                else:
+                    return{"massage":"not patient "} 
         else:
-             return{"error":"already  found"}
-      
+                            return{"error":"already  found"}  
 # @app.route("/addpatient/<id>", methods=["PUT"])  
 # def add_patient(id):
 #     patient=Patient.query.get(id)
@@ -104,6 +150,11 @@ def updata_pat(id):
     name=request.json['name']
     age=request.json['age']
     phone=request.json['phone']
+    h1=request.json['h1']
+    h2=request.json['h2']
+    h3=request.json['h3']
+    h4=request.json['h4']
+    h5=request.json['h5']
     Albumin=request.json['Albumin']
     Platelets=request.json['Platelets']
     AFP=request.json['AFP']
@@ -124,7 +175,8 @@ def updata_pat(id):
     db.session.commit()
     # return{'id':patient.id,'name':patient.name,'age':patient.age} 
     return {'id':patient.id,'name':patient.name,'age':patient.age,
-        'phone':patient.phone,'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,'ALP':patient.ALP,
+        'phone':patient.phone,'h1':patient.h1,'h2':patient.h2,'h3':patient.h3,'h4':patient.h4,'h5':patient.h5,
+        'Albumin':patient.Albumin,'Platelets':patient.Platelets,'AFP':patient.AFP,'ALP':patient.ALP,
         'MCV':patient.MCV,'Iron':patient.Iron
         ,"massage":"the patient is updated"}    
 if __name__ == "__main__":
